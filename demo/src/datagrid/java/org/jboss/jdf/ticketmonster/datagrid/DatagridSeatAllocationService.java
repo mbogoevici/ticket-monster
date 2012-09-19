a3b20524c7ca5d75d56dc1937aa3a60179c590a4
Marius Bogoevici <marius.bogoevici@gmail.com>
2012-09-19 08:56:47 -0400

    Starting on the JDG service

---
+++ demo/src/datagrid/java/org/jboss/jdf/ticketmonster/datagrid/DatagridSeatAllocationService.java
@@ -0,0 +1,33 @@
+package org.jboss.jdf.ticketmonster.datagrid;
+
+import java.util.List;
+
+import javax.enterprise.inject.Alternative;
+
+import org.jboss.jdf.example.ticketmonster.model.Performance;
+import org.jboss.jdf.example.ticketmonster.model.Seat;
+import org.jboss.jdf.example.ticketmonster.model.Section;
+import org.jboss.jdf.example.ticketmonster.service.AllocatedSeats;
+import org.jboss.jdf.example.ticketmonster.service.SeatAllocationService;
+
+/**
+ * @author Marius Bogoevici
+ */
+@Alternative
+public class DatagridSeatAllocationService implements SeatAllocationService {
+
+    @Override
+    public AllocatedSeats allocateSeats(Section section, Performance performance, int seatCount, boolean contiguous) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void deallocateSeats(Section section, Performance performance, List<Seat> seats) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void finalizeAllocation(AllocatedSeats allocatedSeats) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
